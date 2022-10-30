d9b57cd77b018ee580f6a8401d4be320cf0b2cbc
Lasith9812 <lasiththilina9812@gmail.com>
2022-10-30 08:03:44 +0530

    add styles.css

--- get_url.js
+++ get_url.js
@@ -1,14 +1,14 @@
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-    // Do something
+var tab = "hello"
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     var tab = tabs[0];
-    const sender = document.getElementsByClassName("go");
     document.getElementById("las").innerHTML = tab.url;
 });
-
 
 var script = document.createElement('script');
 script.src = "assets/jq.js";
 document.getElementsByTagName('head')[0].appendChild(script);
 
-var url = $.get("http")
-document.getElementById("pl1").innerHTML = url;
+document.getElementById("check_a").addEventListener("click", check_a());
+function check_a(){
+  console.log(tab);
+}
