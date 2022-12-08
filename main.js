c7000bcc98d54ff838cdf9728b3dc1386ed954c8
Lasith9812 <lasiththilina9812@gmail.com>
2022-12-08 23:13:16 +0530

    final

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 checkInSource = function(word){
   var query = word.selectionText;
-  chrome.tabs.create({url: "http://127.0.0.1:8800/search/?name=" + query});
+  chrome.tabs.create({url: "http://34.124.227.193:8800/?name=" + query});
+};
+
+checkInSourceL = function(word){
+  var query = word.linkUrl;
+  chrome.tabs.create({url: "http://34.124.227.193:8800/?name=" + query});
 };
 
 chrome.contextMenus.create({
-  title: "Check url in InSource Spam Detector",
+  title: "Check selected url in InSource Spam Detector",
   contexts:["selection"],
   onclick: checkInSource
 });
 
-check_btn_click = function(){
-  var tab_url = document.getElementById("las").innerHTML;
-  chrome.tabs.create({url: "http://127.0.0.1:8800/search/?name=" + tab_url});
-}
+chrome.contextMenus.create({
+  title: "Check url in InSource Spam Detector",
+  contexts:["link"],
+  onclick: checkInSourceL
+});
